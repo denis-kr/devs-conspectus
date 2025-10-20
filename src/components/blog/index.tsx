5b59163e7a41e2d3833f9c26a3ba97bbd1b4c2c4
denis-kr <kravchdennis1@gmail.com>
2025-10-20 13:57:42 -0400

    adding initial version of blogs

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,5 +1,78 @@
 //TODO this is a Blog Card
 
-export default async function Blog() {
-  return <div></div>;
+// {
+//     id: 1,
+//     title: 'Boost your conversion rate',
+//     href: '#',
+//     description:
+//       'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+//     date: 'Mar 16, 2020',
+//     datetime: '2020-03-16',
+//     category: { title: 'Marketing', href: '#' },
+//     author: {
+//       name: 'Michael Foster',
+//       role: 'Co-Founder / CTO',
+//       href: '#',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     },
+//   },
+
+type BlogProps = {
+  description: string;
+  title: string;
+  href: string;
+  date: string;
+  category: { title: string; href: string };
+  datetime: string;
+};
+
+export default async function Blog({
+  description,
+  title,
+  href,
+  date,
+  category,
+  datetime,
+}: BlogProps) {
+  return (
+    <article className="flex max-w-xl flex-col items-start justify-between">
+      <div className="flex items-center gap-x-4 text-xs">
+        <time dateTime={datetime} className="text-gray-400">
+          {date}
+        </time>
+        <a
+          href={category.href}
+          className="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-gray-300 hover:bg-gray-800"
+        >
+          {category.title}
+        </a>
+      </div>
+      <div className="group relative grow">
+        <h3 className="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
+          <a href={href}>
+            <span className="absolute inset-0" />
+            {title}
+          </a>
+        </h3>
+        <p className="mt-5 line-clamp-3 text-sm/6">{description}</p>
+      </div>
+      {/* <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                <img
+                    alt=""
+                    src={post.author.imageUrl}
+                    className="size-10 rounded-full bg-gray-800"
+                />
+                <div className="text-sm/6">
+                    <p className="font-semibold text-white">
+                        <a href={post.author.href}>
+                            <span className="absolute inset-0" />
+                            {post.author.name}
+                        </a>
+                    </p>
+                    <p className="text-gray-400">{post.author.role}</p>
+                </div>
+            </div> */}
+    </article>
+  );
 }
